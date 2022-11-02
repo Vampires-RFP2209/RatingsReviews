0e60dda59434ec4a774a0ffbc11f524fc44e9b60
KateFeaster <zencornhopper@gmail.com>
2022-11-01 18:08:32 -0700

    Changed column names to match csvs

--- src/db.js
+++ src/db.js
@@ -10,10 +10,11 @@ const reviewSchema = new mongoose.Schema(
     recommend: { type: Boolean, required: true },
     body: { type: String, required: true, maxLength: 1000 },
     reviewer_name: { type: String, required: true, maxLength: 60 },
-    products_id: { type: Number, required: true },
-    email: { type: String, required: true, maxLength: 60 },
+    product_id: { type: Number, required: true },
+    reviewer_email: { type: String, required: true, maxLength: 60 },
     helpfulness: { type: Number, required: true },
-    is_reported: { type: Boolean, default: false },
+    reported: { type: Boolean, default: false },
+    response: String,
     date: {
       type: Date,
       default: () => {
