a288d90353f51099988d4e6c780604ab7beef225
KateFeaster <zencornhopper@gmail.com>
2022-11-03 14:23:44 -0700

    Added characteristics transformer and make cleanAndJSONifyFiles return a promise so that I could run each clean one after another

--- src/mongodb/transformData.js
+++ src/mongodb/transformData.js
@@ -4,25 +4,28 @@ const path = require('path');
 const { Transform } = require('stream');
 
 const cleanAndJSONifyFiles = (filename, transformer) => {
-  console.log(`Now processing ${filename}`);
-  const readStream = fs.createReadStream(path.join(__dirname, `../../datafiles/${filename}`));
-  const writeStream = fs.createWriteStream(
-    path.join(__dirname, `../../datafiles/cleanedFiles/${filename}`)
-  );
+  return new Promise((resolve) => {
+    console.log(`Now processing ${filename}`);
+    const readStream = fs.createReadStream(path.join(__dirname, `../../datafiles/${filename}`));
+    const writeStream = fs.createWriteStream(
+      path.join(__dirname, `../../datafiles/cleanedFiles/${filename}`)
+    );
 
-  readStream
-    .pipe(csv())
-    .pipe(transformer)
-    .pipe(writeStream)
-    .on('end', () => {
-      console.log('successfully cleaned and imported file');
-    });
+    readStream
+      .pipe(csv())
+      .pipe(transformer)
+      .pipe(writeStream)
+      .on('finish', () => {
+        console.log('successfully cleaned and imported file');
+        resolve();
+      });
+  });
 };
 
 const reviewTransformer = new Transform({
   objectMode: true,
   transform(chunk, encoding, callback) {
-    console.log(`Processing record ${chunk.id}`);
+    console.log(`Processing review record ${chunk.id}`);
     const reviewDoc = {
       id: chunk.id,
       rating: chunk.rating,
@@ -43,8 +46,6 @@ const reviewTransformer = new Transform({
   },
 });
 
-cleanAndJSONifyFiles('reviews.csv', reviewTransformer);
-
 class BufferedTransform extends Transform {
   constructor(options) {
     super(options);
@@ -56,7 +57,7 @@ class BufferedTransform extends Transform {
 const photoTransformer = new BufferedTransform({
   objectMode: true,
   transform(chunk, encoding, callback) {
-    console.log(`Processing line: ${chunk.id}`);
+    console.log(`Processing photo line: ${chunk.id}`);
     if (this.buffer.length === 0) {
       this.currentIndex = chunk.review_id;
       this.buffer.push(chunk.url);
@@ -65,8 +66,7 @@ const photoTransformer = new BufferedTransform({
       this.buffer.push(chunk.url);
       callback(null, '');
     } else {
-      const output = {};
-      output[this.currentIndex] = this.buffer;
+      const output = { review_id: this.currentIndex, contents: this.buffer };
       this.currentIndex = chunk.review_id;
       this.buffer = [chunk.url];
       callback(null, `${JSON.stringify(output)}\n`);
@@ -74,4 +74,39 @@ const photoTransformer = new BufferedTransform({
   },
 });
 
-cleanAndJSONifyFiles('reviews_photos.csv', photoTransformer);
+const characteristicsReviewsTransformer = new BufferedTransform({
+  objectMode: true,
+  transform(chunk, encoding, callback) {
+    console.log(`Processing characteristic_reviews line: ${chunk.id}`);
+    const characteristicReview = {
+      characteristic_id: chunk.characteristic_id,
+      value: chunk.value,
+    };
+    if (this.buffer.length === 0) {
+      this.currentIndex = chunk.review_id;
+      this.buffer.push(characteristicReview);
+      callback(null, '');
+    } else if (this.currentIndex === chunk.review_id) {
+      this.buffer.push(characteristicReview);
+      callback(null, '');
+    } else {
+      const output = { review_id: this.currentIndex, contents: this.buffer };
+      this.currentIndex = chunk.review_id;
+      this.buffer = [characteristicReview];
+      callback(null, `${JSON.stringify(output)}\n`);
+    }
+  },
+});
+
+const characteristicsTransformer = new Transform({
+  objectMode: true,
+  transform(chunk, encoding, callback) {
+    console.log(`Processing characteristics line: ${chunk.id}`);
+    callback(null, `${JSON.stringify({ id: chunk.id, name: chunk.name })}\n`);
+  },
+});
+
+cleanAndJSONifyFiles('reviews.csv', reviewTransformer)
+  .then(() => cleanAndJSONifyFiles('reviews_photos.csv', photoTransformer))
+  .then(() => cleanAndJSONifyFiles('characteristic_reviews.csv', characteristicsReviewsTransformer))
+  .then(() => cleanAndJSONifyFiles('characteristics.csv', characteristicsTransformer));
