1d8e9612792424a03f47ae1a68e22ba420fd6681
KateFeaster <zencornhopper@gmail.com>
2022-11-02 18:27:48 -0700

    Created cleanAndLoadReviews

---
+++ src/mongodb/transformAndLoadData.js
@@ -0,0 +1,40 @@
+const fs = require('fs');
+const csv = require('csv-parser');
+const path = require('path');
+const { Review } = require('./db');
+
+const cleanAndLoadReviews = () => {
+  const readStream = fs.createReadStream(path.join(__dirname, '../../datafiles/reviews.csv'));
+  const writeStream = fs.createWriteStream(
+    path.join(__dirname, `../../datafiles/logs/review_etl_${new Date().toJSON()}.txt`)
+  );
+
+  readStream
+    .pipe(csv())
+    .on('data', (data) => {
+      console.log(`Processing record ${data.id}`);
+      Review.create({
+        id: data.id,
+        rating: data.rating,
+        summary: data.summary,
+        recommend: data.recommend,
+        body: data.body,
+        reviewer_name: data.reviewer_name,
+        product_id: data.product_id,
+        reviewer_email: data.reviewer_email,
+        helpfulness: data.helpfulness,
+        reported: data.reported,
+        response: data.response,
+        date: new Date(data.date),
+        photos: [],
+        characteristics: [],
+      })
+        .then((result) => writeStream.write(result))
+        .catch((err) => writeStream.write(err));
+    })
+    .on('end', () => {
+      console.log('successfully cleaned and imported reviews');
+    });
+};
+
+cleanAndLoadReviews();
