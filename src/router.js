1ddfcb8be9f387905865eb1941f3c299ed632675
KateFeaster <zencornhopper@gmail.com>
2022-11-04 14:47:43 -0700

    Set up '/reviews' and '/reviews/meta' routes

--- src/router.js
+++ src/router.js
@@ -1,10 +1,31 @@
 const express = require('express');
+const models = require('./mongodb/models');
 
 const router = express.Router();
 
-router.get('/reviews', (req, res) => {});
+router.get('/reviews', (req, res) => {
+  models
+    .getReviews(req.query.product_id, req.query.page, req.query.count, req.query.sort)
+    .then((result) => {
+      res.status(200).json(result);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(400);
+    });
+});
 
-router.get('/reviews/meta', (req, res) => {});
+router.get('/reviews/meta', (req, res) => {
+  models
+    .getMetadata(req.query.product_id)
+    .then((result) => {
+      res.status(200).json(result);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.sendStatus(400);
+    });
+});
 
 router.post('/reviews', (req, res) => {});
 
