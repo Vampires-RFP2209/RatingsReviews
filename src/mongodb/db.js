71ee98a04ebec95c07f1c1e6b86cb15835a4b034
KateFeaster <zencornhopper@gmail.com>
2022-11-03 19:08:02 -0700

    Added index to product_id since a majority of queries will be on that field

--- src/mongodb/db.js
+++ src/mongodb/db.js
@@ -17,7 +17,7 @@ const reviewSchema = new mongoose.Schema(
     recommend: { type: Boolean, required: true },
     body: { type: String, required: true, maxLength: 1000 },
     reviewer_name: { type: String, required: true, maxLength: 60 },
-    product_id: { type: Number, required: true },
+    product_id: { type: Number, required: true, index: true },
     reviewer_email: { type: String, required: true, maxLength: 60 },
     helpfulness: { type: Number, default: 0 },
     reported: { type: Boolean, default: false },
