1d8f9c55d83af9e6db1dceea4b53807894873afb
KateFeaster <zencornhopper@gmail.com>
2022-11-03 17:24:36 -0700

    Changed to promise style

--- src/mongodb/loadData.js
+++ src/mongodb/loadData.js
@@ -3,27 +3,30 @@ const path = require('path');
 const { Review } = require('./db');
 
 const importFileToDB = (filename, insertionFunc) => {
-  const lineReader = new LineByLineReader(
-    path.join(__dirname, `../../datafiles/cleanedFiles/${filename}`)
-  );
+  return new Promise((resolve) => {
+    const lineReader = new LineByLineReader(
+      path.join(__dirname, `../../datafiles/cleanedFiles/${filename}`)
+    );
 
-  let currentLine = 1;
+    let currentLine = 1;
 
-  lineReader.on('error', (err) => {
-    console.log(err);
-  });
-
-  lineReader.on('line', (line) => {
-    lineReader.pause();
-    console.log(`Processing line: ${currentLine}`);
-    insertionFunc(line).then(() => {
-      lineReader.resume();
-      currentLine += 1;
+    lineReader.on('error', (err) => {
+      console.log(err);
     });
-  });
 
-  lineReader.on('end', () => {
-    console.log('Finsihed importing files');
+    lineReader.on('line', (line) => {
+      lineReader.pause();
+      console.log(`Processing line: ${currentLine}`);
+      insertionFunc(line).then(() => {
+        lineReader.resume();
+        currentLine += 1;
+      });
+    });
+
+    lineReader.on('end', () => {
+      console.log('Finsihed importing files');
+      resolve();
+    });
   });
 };
 
@@ -31,4 +34,4 @@ const reviewInserter = (line) => {
   return Review.create(JSON.parse(line));
 };
 
-importFileToDB('reviews.csv', reviewInserter);
+importFileToDB('reviewsMerged.csv', reviewInserter);
